c8cdf3033e301181b1c847c46401f72206ae4732
Alam Sierra <118128731+sierra-bc@users.noreply.github.com>
2022-11-16 18:37:45 -0400

    add stories

---
+++ src/stories/Test.stories.js
@@ -0,0 +1,10 @@
+import Header from "../components";
+
+export default {
+  // title: "Header",
+  component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const Primary = Template.bind({});
